a1b2694a53cc1befe3799fb890749f489ffdff96
fiqriachmada <achmada1998@gmail.com>
2024-06-03 19:44:44 +0700

    fixed

--- configs/database.js
+++ configs/database.js
@@ -29,6 +29,7 @@ const sequelize = new Sequelize(
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+    
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
