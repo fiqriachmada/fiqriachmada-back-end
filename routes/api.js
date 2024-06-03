ed0fd6794c19b3b30fbab5f034fad1651c3a46fb
fiqriachmada <achmada1998@gmail.com>
2024-06-03 15:54:06 +0700

    add nodemon

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.json({
+    users: [
+      { name: "Admin CP" },
+      { name: "Admin CPx" },
+    ],
+  });
+});
+
+module.exports = router;
