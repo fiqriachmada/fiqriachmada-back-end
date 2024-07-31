a0fa88b4be06fea4d89dc4f27c5346f20993c539
fiqriachmada <achmada1998@gmail.com>
2024-07-31 13:13:20 +0700

    🐞 fix: "Added cors dependency and middleware to api routes"

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 var express = require("express");
+var cors = require("cors");
 
 const workRouter = require("./works/workRouter");
 const userRouter = require("./users/userRouter");
 // const db = require('./../models/index')
 var api = express();
+
+api.use(cors())
 
 api.use(workRouter);
 api.use(userRouter);
